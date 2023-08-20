9e1023d5b4c1337b66996a4aa6fe388de9b5a9e3
aleberrot <berroteranbas@gmail.com>
2023-08-20 17:37:32 -0400

    Data base and register added

--- MY_WEBSITE/config.py
+++ MY_WEBSITE/config.py
@@ -1,5 +1,7 @@
 import os 
-
+basedir = os.path.abspath(os.path.dirname(__file__)) 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Bak1ro_del_m0Nt5'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or  'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLAlCHEMY_TRACK_MODIFICATIONS = False
